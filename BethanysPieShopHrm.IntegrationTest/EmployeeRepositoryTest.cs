6e475b86a995c91afbab3d2da8d6febadf39fc43
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2023-11-28 22:31:34 +0100

    commit for service comntainer test

--- BethanysPieShopHrm.IntegrationTest/EmployeeRepositoryTest.cs
+++ BethanysPieShopHrm.IntegrationTest/EmployeeRepositoryTest.cs
@@ -1,21 +1,21 @@
 ﻿namespace BethanysPieShopHrm.IntegrationTest
 {
-    [Collection("Database Collection")]
-    public class EmployeeRepositoryTest
-    {
-        private readonly DatabaseFixture _fixture;
+	[Collection("Database Collection")]
+	public class EmployeeRepositoryTest
+	{
+		private readonly DatabaseFixture _fixture;
 
-        public EmployeeRepositoryTest(DatabaseFixture fixture)
-        {
-            _fixture = fixture;
-        }
+		public EmployeeRepositoryTest(DatabaseFixture fixture)
+		{
+			_fixture = fixture;
+		}
 
-        [Fact]
-        public void Should_create_Database() 
-        {
-          var employee =   _fixture.EmployeeRepository.GetAllEmployees();
+		[Fact]
+		public void Should_create_Database()
+		{
+			var employee = _fixture.EmployeeRepository.GetAllEmployees();
 
-            Assert.Equal(true, true);
-        }
-    }
+			Assert.Single(employee);
+		}
+	}
 }
